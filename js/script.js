bcf8e24f762eb2223c4554e77cd6222e9e128714
Moseley <amoseley@unityprep.org>
2019-05-16 22:30:17 +0000

    DAY 1

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
+ $('#searchButton').click(function() {
+        var searchTerm = $("#searchTerm").val();
+        var Url = "https://openweathermap.org/api"
+function getWeather (){
+  $.ajax({
+          url: giphyUrl,
+          method: "GET",
+          success: function(response) {
+
+
+
+}
+
+

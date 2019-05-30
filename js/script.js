ea49b8d2580b5cb2bca1282883816e17f0130741
Andrew Meier <ameier38@gmail.com>
2019-05-30 13:56:14 +0000

    suggested changes

--- js/script.js
+++ js/script.js
@@ -6,17 +6,17 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
- $('#searchButton').click(function() {
-        var searchTerm = $("#searchTerm").val();
-        var Url = "https://openweathermap.org/api"
-function getWeather (){
-  $.ajax({
-          url: giphyUrl,
-          method: "GET",
-          success: function(response) {
 
-
-
-}
-
-
+$('#searchButton').click(function() {
+    var searchTerm = $("#searchTerm").val();
+    // replace spaces with encoded space
+    var formattedSearchTerm = searchTerm.replace(' ', '%20')
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3c29e935498d124ef58372d0141ec744";
+    $.ajax({
+        url: url,
+        method: "GET",
+        success: function(response) {
+            console.log('response', response)
+        }
+    })
+})
